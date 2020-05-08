5758256f6fb7901ac2d138e4722feed0cbc52fe1
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-08 12:02:11 +0900

    20200508_commit

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -10,6 +10,12 @@ namespace csloop
     {
         static void Main(string[] args)
         {
+            long start = DateTime.Now.Ticks;
+            long count = 0;
+            while (start + (10000000) > DateTime.Now.Ticks)){
+                count++;
+            }
+            Console.Write(count + "만큼 반복함");
         }
     }
 }
